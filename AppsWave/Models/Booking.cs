61473642b0c4e6914a0702bb84ece17405b9a02e
Abdallah Sabha <aboodsabha78@gmail.com>
2026-08-31 20:47:53 +0300

    Add the booking model and status enum

--- AppsWave/Models/Booking.cs
+++ AppsWave/Models/Booking.cs
@@ -1,6 +1,13 @@
-﻿namespace AppsWave.Models
+﻿namespace AppsWave.Models;
+public class Booking
 {
-    public class Booking
-    {
-    }
+    public int Id { get; set; }
+    public string ResourceId { get; set; } = "";
+    public string UserId { get; set; } = "";
+    public DateTime CreatedAt { get; set; }
+    public DateTime? CancelledAt { get; set; } = null;
+    public DateTime StartDateTime { get; set; }
+    public DateTime EndDateTime { get; set; }
+    public BookingStatus Status { get; set; } = BookingStatus.Active;
+
 }
